62de094ea7f351b4d765739d051b57a9fdd021bf
qbreis <qbreis@gmail.com>
2022-07-14 16:44:28 +0200

    feat: add site title in all pages

--- components/MetaData.tsx
+++ components/MetaData.tsx
@@ -6,11 +6,12 @@ import nextConfig from '../next.config';
 
 export default function MetaData ({title, description,}: any){
     const staticStylesCss = '../'.repeat(useRouter().asPath.split('/').length) + 'static-styles.css';
+    console.log(title);
     return (
         <Head>
             <meta charSet='utf-8' />
             <meta name='viewport' content='width=device-width, initial-scale=1' />
-            <title>{title}</title>
+            <title>{(title !== nextConfig.siteInfo.title) ? nextConfig.siteInfo.title + ' | ' + title : title}</title>
             <meta name="description" content={description} />
             <link href={staticStylesCss} rel="stylesheet" />
             <link rel="icon" href="/images/logo.svg" />
