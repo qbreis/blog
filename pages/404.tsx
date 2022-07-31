5cf46906deab33aea0bb7a5aa2a5aea26955f807
qbreis <qbreis@gmail.com>
2022-07-31 09:40:17 +0000

    fix: page 404 2nd try

--- pages/404.tsx
+++ pages/404.tsx
@@ -9,17 +9,6 @@ export default function Custom404() {
             <MetaData title="404 - Page Not Found" />
             <section>
                 <h1>404</h1>
-                <div className="entry-meta posted-on">
-                    Page Not Found
-                </div>
-                <div className="post-categories">
-                    <Link href='/'>
-                        <a>home page</a>
-                    </Link>
-                    <Link href='/categories'>
-                        <a>categories</a>
-                    </Link>
-                </div>
 
             </section>
         </Layout>
