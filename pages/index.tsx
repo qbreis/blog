c38bf32a51efe85e8dc79cf92be41938d9199692
Enric <qbreis@gmail.com>
2022-09-15 12:08:41 +0000

    feat: import components into each page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,15 @@
 import Link from 'next/link';
-
-import Header from '../components/Header'; /* 1 */
+import Layout from '../components/Layout';
 
 export default function Home() {
   return (
-    <div className="site-container">
-      <header className="site-header">site-header</header>
-      <main className="site-main">
-        <article>
-          <h1>Home</h1>
-          <Link href="/custom-template">
-            <a>Custom template</a>
-          </Link>
-        </article>
-      </main>
-      <footer className="site-footer">site-footer</footer>
-    </div>
+    <Layout>
+      <section>
+        <h1>Home</h1>
+        <Link href="/custom-template">
+          <a>Custom template</a>
+        </Link>
+      </section>
+    </Layout>
   );
 }
