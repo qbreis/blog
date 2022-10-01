a900f57875504a671e9f4cd93539da08b1230621
Enric <qbreis@gmail.com>
2022-10-01 05:57:47 +0000

    feat: change arrow function for function declaration

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,3 @@
-import type { NextPage } from 'next';
-// import Head from 'next/head';
-// import Image from 'next/image';
-// import styles from '../styles/Home.module.css';
-
-const Home: NextPage = () => {
+export default function Home() {
   return <>Hola</>;
-};
-
-export default Home;
+}
