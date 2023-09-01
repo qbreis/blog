bddd4750835bfefb7a1fdff78534bb0bb8d2523c
qbreis <qbreis@gmail.com>
2023-09-01 17:47:12 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -33,6 +33,7 @@ const nextConfig = {
             {
               key: 'Referrer-Policy',
               value: 'origin-when-cross-origin',
+              
             },
           ],
         },
