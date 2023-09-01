35ed48bc46e53faffd1ea64c840b30602d062ee4
qbreis <qbreis@gmail.com>
2023-09-01 17:46:19 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,6 +6,40 @@ Many of these annotations are related to their corresponding Git repository.`;
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+
+  // Adding policies:
+  async headers() {
+    return [
+        {
+          source: '/(.*)',
+          headers: [
+            {
+              key: 'X-Frame-Options',
+              value: 'DENY',
+            },
+            {
+              key: 'Content-Security-Policy',
+              value:
+                "default-src 'self' 'https://blog-qbreis.vercel.app/'; image-src 'https://blog-qbreis.vercel.app/'; script-src 'self' https://www.google-analytics.com; font-src 'self' 'https://fonts.googleapis.com'",
+            },
+            {
+              key: 'X-Content-Type-Options',
+              value: 'nosniff',
+            },
+            {
+              key: 'Permissions-Policy',
+              value: "camera=(); battery=(self); geolocation=();",
+            },
+            {
+              key: 'Referrer-Policy',
+              value: 'origin-when-cross-origin',
+            },
+          ],
+        },
+      ];
+  },  
+
+  
   env: {
     siteInfoTitle: siteInfoTitle,
     siteInfoDescription: siteInfoDescription,
